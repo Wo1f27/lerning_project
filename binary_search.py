6f3ea16d4d5e5b033862bdea9cc84b030ecec417
Wo1f27 <avstrelkov@yandex.ru>
2023-11-30 22:45:12 +0300

    Update file

--- binary_search.py
+++ binary_search.py
@@ -14,4 +14,5 @@ def binary_search(arr, target):
 
 
 if __name__ == '__main__':
-    print(binary_search([1, 2, 4, 6, 23, 434, 453, 1231, 1235, 5600, 6644, 7123], 7123))
+    print(binary_search([1, 2, 4, 6, 23, 434, 453, 123, 1230, 5600, 6600, 7123], 7123))
+    print(binary_search([1, 2, 4, 6, 23, 434, 453, 523, 1230, 5600, 6600, 7123], 523))
