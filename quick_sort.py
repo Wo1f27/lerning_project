addb2e71a38c191fbfb8a564ab40ba32f86edf26
Wo1f27 <avstrelkov@yandex.ru>
2023-12-02 11:18:13 +0300

    Add file

---
+++ quick_sort.py
@@ -0,0 +1,11 @@
+def qsort(arr):
+    if len(arr) < 2:
+        return arr
+    first = arr[0]
+    less = [i for i in arr[1:] if i <= first]
+    greater = [i for i in arr[1:] if i > first]
+    return qsort(less) + [first] + qsort(greater)
+
+
+a = [6, 3, 9, 1, 5, 5, 6, 2, 7, 1]
+print(qsort(a))
